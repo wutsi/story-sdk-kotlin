4b3c7a6fcd6e985f23abc53995e0da3bd7c264b1
htchepannou <htchepannou@expedia.com>
2021-04-05 18:06:24 -0400

    add additional field on story

--- src/main/kotlin/com/wutsi/story/dto/Story.kt
+++ src/main/kotlin/com/wutsi/story/dto/Story.kt
@@ -6,8 +6,10 @@ import kotlin.String
 public data class Story(
   public val id: Long = 0,
   public val siteId: Long = 0,
+  public val userId: Long = 0,
   public val title: String = "",
-  public val tagName: String = "",
+  public val tagLine: String = "",
   public val summary: String = "",
-  public val slug: String = ""
+  public val slug: String = "",
+  public val socialMediaMessage: String? = null
 )
