ee335079858f81344b70235e07f25d12d652a73a
htchepannou <htchepannou@expedia.com>
2021-04-10 06:46:13 -0400

    add interceptor

--- src/main/kotlin/com/wutsi/story/StoryApiBuilder.kt
+++ src/main/kotlin/com/wutsi/story/StoryApiBuilder.kt
@@ -1,12 +1,19 @@
 package com.wutsi.story
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import feign.RequestInterceptor
+import kotlin.collections.List
 
 public class StoryApiBuilder {
-  public fun build(env: Environment, mapper: ObjectMapper) = feign.Feign.builder()
+  public fun build(
+    env: Environment,
+    mapper: ObjectMapper,
+    interceptors: List<RequestInterceptor> = emptyList()
+  ) = feign.Feign.builder()
     .client(feign.okhttp.OkHttpClient())
     .encoder(feign.jackson.JacksonEncoder(mapper))
     .decoder(feign.jackson.JacksonDecoder(mapper))
     .logger(feign.slf4j.Slf4jLogger())
+    .requestInterceptors(interceptors)
     .target(StoryApi::class.java, env.url)
 }
