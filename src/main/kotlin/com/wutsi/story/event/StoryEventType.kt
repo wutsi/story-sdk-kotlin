f030f8ae4bf5f917fc087949ef02b114bafd10c1
htchepannou <htchepannou@expedia.com>
2021-06-02 09:43:32 -0400

    add feed-updated event

--- src/main/kotlin/com/wutsi/story/event/StoryEventType.kt
+++ src/main/kotlin/com/wutsi/story/event/StoryEventType.kt
@@ -1,5 +1,6 @@
 package com.wutsi.story.event
 
 enum class StoryEventType(val urn: String) {
-  PUBLISHED("urn:event:wutsi:story:published")
+  PUBLISHED("urn:event:wutsi:story:published"),
+  FEED_UPDATED("urn:event:wutsi:story:feed-updated")
 }
