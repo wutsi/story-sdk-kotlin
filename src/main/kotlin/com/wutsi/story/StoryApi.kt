6014950a403ac4714bf0891d801e45e3661552d0
htchepannou <htchepannou@expedia.com>
2021-06-28 05:06:57 -0400

    translate

--- src/main/kotlin/com/wutsi/story/StoryApi.kt
+++ src/main/kotlin/com/wutsi/story/StoryApi.kt
@@ -3,17 +3,24 @@ package com.wutsi.story
 import com.wutsi.story.dto.GetStoryResponse
 import com.wutsi.story.dto.SearchStoryResponse
 import com.wutsi.story.dto.SearchTopicResponse
+import com.wutsi.story.dto.TranslateStoryResponse
 import feign.Headers
 import feign.Param
 import feign.RequestLine
 import java.time.LocalDate
 import kotlin.Int
 import kotlin.Long
+import kotlin.String
 
 public interface StoryApi {
   @RequestLine("GET /v1/story/{id}")
   @Headers("Content-Type: application/json")
   public fun `get`(@Param("id") id: Long): GetStoryResponse
+
+  @RequestLine("GET /v1/story/{id}/translate?language={language}")
+  @Headers("Content-Type: application/json")
+  public fun translate(@Param("id") id: Long, @Param("language") language: String):
+      TranslateStoryResponse
 
   @RequestLine("GET /v1/stories/published?site-id={site-id}&start-date={start-date}&end-date={end-date}&limit={limit}&offset={offset}")
   @Headers("Content-Type: application/json")
---
+++ src/main/kotlin/com/wutsi/story/dto/TranslateStoryResponse.kt
@@ -0,0 +1,5 @@
+package com.wutsi.story.dto
+
+public data class TranslateStoryResponse(
+  public val story: Story = Story()
+)
